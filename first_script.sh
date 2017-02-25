6c8ebfeea2bf19cb97a6c3f23b8bc440d294e324
Gross <davidcgross@email.arizona.edu>
2017-02-25 12:30:20 -0700

    Add help

--- first_script.sh
+++ first_script.sh
@@ -1,2 +1,7 @@
+if [ "$1" == "--help" ] || [ $1 == "-h" ]; then
+   echo "Usage: sh first_script.sh input_file output_file"
+   exit
+fi
+
 #This script sends size of first five lines of first arg to second arg 
 head -5 $1 | wc -c > $2
